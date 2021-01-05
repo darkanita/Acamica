eb94c94fdb8b9c6e664b36678ba984145765b42b
Ana Maria Lopez Moreno <anamaria.lopez@globant.com>
2021-01-05 18:33:04 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Uso de Git para proyectos en ciencia de datos',
+    author='Ana Lopez',
+    license='',
+)
